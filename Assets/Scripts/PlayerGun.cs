59297b6e4e74a034f8d4135cab26134c1bb43aa4
MuhammedWaleedPTK <muhammedwaleedkidanhi@gmail.com>
2023-03-19 21:57:21 +0530

    changed the PlayerGun script for better firing

--- Assets/Scripts/PlayerGun.cs
+++ Assets/Scripts/PlayerGun.cs
@@ -6,19 +6,28 @@ public class PlayerGun : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     //[SerializeField] private GameObject bulletPrefab;
+    bool isFired = true;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R)&&isFired)
         {
-            GameObject bullet = ObjectPool.instance.GetPooledObject();
-            if(bullet != null )
-            {
-                bullet.transform.position= firePoint.position;
-                bullet.transform.rotation = firePoint.rotation;
-                bullet.SetActive(true);
-            }
-          
+            StartCoroutine(Fire());
+            isFired = false;          
         }
+    }
+    private IEnumerator Fire()
+    {
+        yield return new WaitForSeconds(.3f);
+        GameObject bullet = ObjectPool.instance.GetPooledObject();
+        if (bullet != null)
+        {
+            bullet.transform.position = firePoint.position;
+            bullet.transform.rotation = firePoint.rotation;
+            bullet.SetActive(true);
+        }
+        yield return new WaitForSeconds(.5f);
+        isFired = true;
+
     }
 }
