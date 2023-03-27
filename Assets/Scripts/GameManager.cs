22a12809289f93a4f50dc478623f2ee6e2345808
MuhammedWaleedPTK <muhammedwaleedkidanhi@gmail.com>
2023-03-27 15:08:41 +0530

    added animation for startin menu and all panels in game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,44 +16,67 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        playerMovement=FindObjectOfType<PlayerMovement>();
-        playerGun=FindObjectOfType<PlayerGun>();
-        itemCollector=FindObjectOfType<ItemCollecter>();
-        if (SceneManager.GetActiveScene().name == "Level01")
-        {
-            PlayerPrefs.SetInt("HighScore", 0);
-        }
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        playerGun = FindObjectOfType<PlayerGun>();
+        itemCollector = FindObjectOfType<ItemCollecter>();
+
+        
+
+        // Get the number of unlocked levels and the index of the current scene
         int unlockedLevels = PlayerPrefs.GetInt("UnlockedLevels", 0);
         int scene = SceneManager.GetActiveScene().buildIndex;
+
+        // If the current scene is further than the number of unlocked levels, update the unlocked level count
         if (unlockedLevels < scene)
         {
             PlayerPrefs.SetInt("UnlockedLevels", SceneManager.GetActiveScene().buildIndex);
         }
-
     }
+
+    // Reloads the current scene
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    // Called when the player wins the game
     public void GameWon()
     {
-        CoinCount.text="COINS:"+itemCollector.collected.ToString();
-        PlayerPrefs.SetInt("HighScore",PlayerPrefs.GetInt("HighScore",0)+itemCollector.collected);
-        totalCount.text = "TOTAL:" + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        // Update the coin count and total score
+        CoinCount.text = "COINS: " + itemCollector.collected.ToString();
+        PlayerPrefs.SetInt("HighScore", PlayerPrefs.GetInt("HighScore", 0) + itemCollector.collected);
+        totalCount.text = "TOTAL: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
+
+        // Show the game won panel and disable player movement and firing
         GameWonPanel.SetActive(true);
         playerMovement.isPlayable = false;
         playerGun.isFirable = false;
+
+        // Update the unlocked level count if the current scene is further than the previous count
+        int unlockedLevels = PlayerPrefs.GetInt("UnlockedLevels", 0);
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        if (unlockedLevels < scene)
+        {
+            PlayerPrefs.SetInt("UnlockedLevels", SceneManager.GetActiveScene().buildIndex+1);
+        }
     }
+
+    // Called when the player loses the game
     public void GameLost()
     {
+        // Show the game over panel and disable player movement and firing
         GameOverPanel.SetActive(true);
         playerMovement.isPlayable = false;
         playerGun.isFirable = false;
     }
+
+    // Loads the main menu scene
     public void ToMenu()
     {
         SceneManager.LoadScene("Menu");
     }
+
+    // Loads the next level in the build order
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
